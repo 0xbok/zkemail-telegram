2a399c6c130716f4e491f679b87f6e09d71d9c9b
bunbun295 <1689531+0xbok@users.noreply.github.com>
2024-11-17 05:29:29 +0700

    private chat

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# zkemail-tg
+# A zkemail gated access to a telegram group
+
+> Incomplete project
+
+Idea: Prove that you received YC rejection mail but were in the top 10% of applicants (YC includes this information in the rejection mail). If proven, you can join a telegram group. Immediately become part of the motivated founder group, share advice, team up via the group chat.
+
+## Current status
+Lot of time went into telegram specific setup during the hackathon.
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use url::Url;
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase", description = "These commands are supported:")]
 enum Command {
-    #[command(description = "Invite to YC group")]
+    #[command(description = "Alert")]
     Invite,
 }
 
@@ -26,34 +26,33 @@ async fn main() {
     Command::repl(bot, move |bot: Bot, msg: Message, cmd: Command| async move {
         match cmd {
             Command::Invite => {
-                if let ChatKind::Private(_) = msg.chat.kind {
-                    if msg.from().is_some() && allow_invite() {
-                        match bot.create_chat_invite_link(ChatId(group_id))
-                            .member_limit(1)
-                            .await
-                        {
-                            Ok(invite_link) => {
-                                match Url::parse(&invite_link.invite_link) {
-                                    Ok(url) => {
-                                        let keyboard = InlineKeyboardMarkup::new(vec![vec![
-                                            InlineKeyboardButton::url("Join YC Group", url)
-                                        ]]);
+                let is_chat_private = matches!(msg.chat.kind, ChatKind::Private(_));
+                if msg.from().is_some() && is_chat_private && allow_invite() {
+                    match bot.create_chat_invite_link(ChatId(group_id))
+                        .member_limit(1)
+                        .await
+                    {
+                        Ok(invite_link) => {
+                            match Url::parse(&invite_link.invite_link) {
+                                Ok(url) => {
+                                    let keyboard = InlineKeyboardMarkup::new(vec![vec![
+                                        InlineKeyboardButton::url("Join YC Group", url)
+                                    ]]);
 
-                                        bot.send_message(msg.chat.id, "Click the button below to join the YC group:")
-                                            .reply_markup(keyboard)
-                                            .await?;
-                                    },
-                                    Err(_) => {
-                                        bot.send_message(msg.chat.id, "Sorry, there was an error processing the invite link.")
-                                            .await?;
-                                    }
+                                    bot.send_message(msg.chat.id, "Click the button below to join the YC group:")
+                                        .reply_markup(keyboard)
+                                        .await?;
+                                },
+                                Err(_) => {
+                                    bot.send_message(msg.chat.id, "Sorry, there was an error processing the invite link.")
+                                        .await?;
                                 }
                             }
-                            Err(err) => {
-                                log::error!("Failed to create invite link: {:?}", err);
-                                bot.send_message(msg.chat.id, "Sorry, I couldn't create an invite link. Please try again later.")
-                                    .await?;
-                            }
+                        }
+                        Err(err) => {
+                            log::error!("Failed to create invite link: {:?}", err);
+                            bot.send_message(msg.chat.id, "Sorry, I couldn't create an invite link. Please try again later.")
+                                .await?;
                         }
                     }
                 }
